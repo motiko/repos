43bd8841ea1364319cbe2357fc684d9a9cc3332a
Moti Korets <moti.korets@riskmethods.net>
2019-03-18 17:42:35 +0100

    Updates

---
+++ precache-manifest.48d2a4c5c28d889b164da97a97dd61fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcdfdc659be3b24fe3aa",
+    "url": "/repos/static/js/runtime~main.be4aa5e7.js"
+  },
+  {
+    "revision": "4cdbd750863c2512e561",
+    "url": "/repos/static/js/main.4d0621fb.chunk.js"
+  },
+  {
+    "revision": "ab83ffe8bff741c7379b",
+    "url": "/repos/static/js/2.86b803a3.chunk.js"
+  },
+  {
+    "revision": "8bc2ce62c9259fc048e5c5573632b892",
+    "url": "/repos/index.html"
+  }
+];
